c9f1b21cdff207136467d9c2140e58d34ea59f4c
ericknovoa <ericknovoa8@gmail.com>
2026-03-01 16:53:27 -0500

    primer commit finanzas

---
+++ finanzas.java
@@ -0,0 +1,49 @@
+public class finanzas {
+    private String nombre;
+    private double ingresos;
+    private double gastosHogar;
+    private double gastosPersonales;
+
+    public finanzas(String nombre, double ingresos, double gastosHogar, double gastosPersonales) {
+        this.nombre = nombre;
+        this.ingresos = ingresos;
+        this.gastosHogar = gastosHogar;
+        this.gastosPersonales = gastosPersonales;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getIngresos() {
+        return ingresos;
+    }
+
+    public void setIngresos(double ingresos) {
+        this.ingresos = ingresos;
+    }
+
+    public double getGastosHogar() {
+        return gastosHogar;
+    }
+
+    public void setGastosHogar(double gastosHogar) {
+        this.gastosHogar = gastosHogar;
+    }
+
+    public double getGastosPersonales() {
+        return gastosPersonales;
+    }
+
+    public void setGastosPersonales(double gastosPersonales) {
+        this.gastosPersonales = gastosPersonales;
+    }
+    public void calcular(){
+        System.out.println("tus ingresos son ");
+        
+    }
+}
